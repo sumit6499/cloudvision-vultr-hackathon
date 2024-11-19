52802e6c601c65e22db48a87a4940d798424a1e1
Sumit <sumitpadalkar53@gmail.com>
2024-11-19 08:50:17 +0530

    add:RAG chat completion

--- src/actions/fetch.ts
+++ src/actions/fetch.ts
@@ -30,14 +30,30 @@ export const createInfra = async (diagramID: string) => {
 export const getChatCompletion = async (message: string) => {
 
  try {
+    const diagramID="fd8ad278-06fb-4576-a9e8-f190f9e61eca"
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/infra/code/${diagramID}`,
+      {
+        method: "GET",
+      }
+    );
+    const responseData=await response.json();
+
+    const terraformCode=responseData.data.terraformCode
+
     const client = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
-    const model = client.getGenerativeModel({ model: "gemini-1.5-flash", systemInstruction: "You are a helpful assistant that can answer questions and help with tasks. Behave as a helpful assistant." });
+    const model = client.getGenerativeModel({ model: "gemini-1.5-flash", systemInstruction: `You are a helpful assistant & terraform with cloud infrasturcture expert that can answer questions and help with tasks by understanding code given in your system instruction after terraform code keyword. Behave as a helpful assistant. terraform code : ${terraformCode}` });
     const result = await model.generateContent(message);
     console.log(result.response.text());
   
     return result.response.text();
+
+
  } catch (error) {
     console.log(error)
  }
  
 };
+
+getChatCompletion("how many instances this in code")
