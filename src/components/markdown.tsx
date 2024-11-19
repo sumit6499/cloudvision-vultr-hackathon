54d5858e6abe9cb18b3361d44e0247995355b4c9
sahil <sahilmulani501@gmail.com>
2024-11-19 15:10:18 +0530

    fix css bug

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -18,7 +18,7 @@ const NonMemoizedMarkdown = ({ children }: { children: string }) => {
         </pre>
       ) : (
         <code
-          className={`${className} text-sm bg-zinc-100 dark:bg-zinc-800 py-0.5 px-1 rounded-md`}
+          className={`${className} text-sm bg-zinc-100 text-black dark:bg-zinc-800 py-0.5 px-1 rounded-md`}
           {...props}
         >
           {children}
