d7b5449997d3a15d9bb82915602ffabb5678c764
Sumit <sumitpadalkar53@gmail.com>
2024-11-19 08:58:00 +0530

    feat:AI insights on created infra

--- src/actions/fetch.ts
+++ src/actions/fetch.ts
@@ -56,4 +56,33 @@ export const getChatCompletion = async (message: string) => {
  
 };
 
-getChatCompletion("how many instances this in code")
+export const getAiInsights= async ()=>{
+  
+ try {
+  const diagramID="fd8ad278-06fb-4576-a9e8-f190f9e61eca"
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/infra/code/${diagramID}`,
+    {
+      method: "GET",
+    }
+  );
+  const responseData=await response.json();
+
+  const terraformCode=responseData.data.terraformCode
+
+  const client = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
+  const model = client.getGenerativeModel({ model: "gemini-1.5-flash", systemInstruction: `You are a helpful assistant & terraform with cloud infrasturcture expert that can answer questions and help with tasks by understanding code given in your system instruction after terraform code keyword. Behave as a Terraform Expert that provide suggestion and insights on infrastucture that will be created after given code executed. Don't provide suggestion on api key or secret parts terraform code : ${terraformCode}` });
+  const result = await model.generateContent("Provide Suggestion and insights");
+  console.log(result.response.text());
+
+  return result.response.text();
+
+
+} catch (error) {
+  console.log(error)
+}
+}
+
+// getChatCompletion("hi how are you")
+getAiInsights()
